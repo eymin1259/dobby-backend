757ac1c37d8109179317f319f6878cfab78049a2
이나현 <lina@inahyeon-ui-MacBookAir.local>
2022-09-03 18:37:53 +0900

    CHORE: 초기세팅_ gitignore추가

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const dotenv = require("dotenv").config();
-const mysqlConnectObj = require("./config/mysql");
+const mysqlConnectObj = require("./src/config/mysql");
 const db = mysqlConnectObj.init();
 
 mysqlConnectObj.open(db);
 
-app.get("/", (req, res) => res.send("hello NH world!"));
-
 app.listen(port, () => console.log("sever running.."));
+
+app.get("/", (req, res) => res.send("hello NH world, home"));
+
+app.get("/about", (req, res) => res.send("hello NH world!, about"));
