22dfda1df55c84c85dcdfb3b781ef88dcea2bb6f
Nahyun Lee <lnhyen43@gmail.com>
2022-09-17 10:42:42 +0900

    FIX: user table sequlize define 수정

--- src/models/user.ts
+++ src/models/user.ts
@@ -4,16 +4,53 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     "User",
     {
-      id: DataTypes.INTEGER,
-      user_name: DataTypes.STRING,
-      user_email: DataTypes.STRING,
-      profile_color: DataTypes.STRING,
-      profile_url: DataTypes.STRING,
-      signup_time: DataTypes.DATETIME,
-      start_time: DataTypes.DATETIME,
-      revise_time: DataTypes.DATETIME,
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        unique: true,
+        comment: "user id",
+      },
+      user_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        comment: "user name",
+      },
+      user_email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        comment: "user email",
+      },
+      profile_color: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        comment: "profile color",
+      },
+      profile_url: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        comment: "profile url",
+      },
+      signup_time: {
+        type: DataTypes.DATETIME,
+        allowNull: true,
+        comment: "signup_time",
+      },
+      start_time: {
+        type: DataTypes.DATETIME,
+        allowNull: true,
+        comment: "signup_time",
+      },
+      revise_time: {
+        type: DataTypes.DATETIME,
+        allowNull: true,
+        comment: "signup_time",
+      },
     },
-    {}
+    {
+      charset: "utf8",
+      collate: "utf8_general_ci", //한글 저장
+    }
   );
-  User.associate = function (models) {};
+  return User;
 };
