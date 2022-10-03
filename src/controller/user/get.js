49c508a0c78e4f2d899f49655eeba93e835dee70
Nahyun Lee <lnhyen43@gmail.com>
2022-10-03 17:35:09 +0900

    CHORE: jwt 삽질..

--- src/controller/user/get.js
+++ src/controller/user/get.js
@@ -2,13 +2,7 @@ const axios = require("axios");
 const jwt = require("jsonwebtoken");
 const { users } = require("../../models");
 
+// 소셜로그인과 로그인 controller 분리
 module.exports = async (req, res) => {
-  // 소셜로그인과 로그인 controller 분리
   res.send("get user");
-  // try {
-  //   const socialAccessToken = req;
-  //   console.log("request", socialAccessToken);
-  // } catch (e) {
-  //   console.log("error", e);
-  // }
 };
