5d990fdf16ddb08821a36ffd2cfec5a2a6ec2e07
Nahyun Lee <lnhyen43@gmail.com>
2022-10-05 00:17:08 +0900

    FEAT: oauth router socialLogin 추가

--- src/controller/oauth/index.js
+++ src/controller/oauth/index.js
@@ -1,4 +1,5 @@
 const socialLogin = require("./socialLogin");
+//kakao랑 구글 redirect url 설정..!(아마)
 
 module.exports = {
   socialLogin,
--- src/router/oauth.js
+++ src/router/oauth.js
@@ -1,2 +1,6 @@
 const router = require("express").Router();
 const { socialLogin } = require("../controller/oauth");
+
+router.post("/login", socialLogin);
+
+module.exports = router;
