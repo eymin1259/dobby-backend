3170f44d2657186b9bf184fb135b96d1f547ba4f
eymin1259 <eymin1259@gmail.com>
2022-10-15 16:59:46 +0900

    FEAT : 포트설정

--- src/router/main.js
+++ src/router/main.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  res.send("hello world by anthony");
+  res.send(`hello world <br>port : ${process.env.PORT}`);
 });
 
 module.exports = router;
