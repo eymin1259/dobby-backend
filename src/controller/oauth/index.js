8a27597f8447cdbfdf46c4547d3f09847dd4fd9f
eymin1259 <eymin1259@gmail.com>
2022-10-16 19:14:22 +0900

    FIX : apple logout -> apple revoke로 이름 수정

--- src/controller/oauth/index.js
+++ src/controller/oauth/index.js
@@ -1,7 +1,7 @@
 const socialLogin = require("./socialLogin");
 const kakaoLogin = require("./kakaoLogin");
 const appleLogin = require("./appleLogin");
-const appleLogout = require("./appleLogout");
+const appleRevoke = require("./appleRevoke");
 const refreshToken = require("./refreshToken");
 //kakao랑 구글 redirect url 설정..!(아마)
 
@@ -9,6 +9,6 @@ module.exports = {
   socialLogin,
   kakaoLogin,
   appleLogin,
-  appleLogout,
+  appleRevoke,
   refreshToken
 };
--- src/router/oauth.js
+++ src/router/oauth.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
-const { socialLogin, kakaoLogin, appleLogin, appleLogout, refreshToken } = require("../controller/oauth");
+const { socialLogin, kakaoLogin, appleLogin, appleRevoke, refreshToken } = require("../controller/oauth");
 
 router.post("/login", socialLogin);
 router.post("/kakaoLogin", kakaoLogin);
 router.post("/appleLogin", appleLogin);
-router.post("/appleLogout", appleLogout);
+router.post("/appleRevoke", appleRevoke);
 router.post("/refreshToken", refreshToken);
 
 module.exports = router;
