3406bbcd729c3e05f8ce50066d84e27dbaa23681
eymin1259 <eymin1259@gmail.com>
2022-10-16 19:40:52 +0900

    FEAT : 카카오탈퇴 구현

---
+++ src/controller/oauth/kakaoRevoke.js
@@ -0,0 +1,44 @@
+const axios = require("axios");
+const fs = require("fs");
+const querystring = require('querystring');
+const jwt = require("jsonwebtoken");
+const UserService = require('../../service/UserService');
+
+module.exports = async (req, res) => {
+    try {
+
+        const userService = new UserService();
+        const userId = req.userId;
+        if (!userId) {
+            // access token 만료
+            res.status(401).send({
+                message: 'invalide access token'
+            });
+        }
+        const user = userService.getUser(userId);
+        const kakaoAcessToken = user.sns_access_token; 
+
+        axios.post(
+            'https://kapi.kakao.com/v1/user/unlink',
+            querystring.stringify({}),
+            {
+                headers: {
+                    "Authorization": `Bearer ${kakaoAcessToken}`,
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            },
+        ).then(function (response) {
+            userService.deleteUser(userId);
+            console.log("revoke success");
+            console.log(response);
+            res.status(200).send();
+        }).catch(function (error) {
+            console.log("revoke fail");
+            console.log(error);
+            res.status(404).send({ message: e.message });
+        });
+        res.status(200).send({  message: 'success' });
+    } catch (e) {
+        res.status(404).send({ message: e.message });
+    }
+};
